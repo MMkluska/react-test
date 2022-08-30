384a876b504f36e9e0b3c3854621e3d63fe0e8ec
Mateusz Kluska <mateusz.marek.kluska@gmail.com>
2022-08-30 15:18:44 +0100

    first update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import './App.css';
 
 
 function App() {
+
+  const myself = {
+    firstName : "Mateusz",
+    lastName : "Kluska",
+    postCode : "E15 4JT",
+    city : "London"
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>{myself.firstName}</h3>
+    <h3>{myself.lastName}</h3>
+    <h3>{myself.city}</h3>
+    <h3>{myself.postCode}</h3>
     </div>
   );
 }
