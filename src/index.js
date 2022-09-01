4fd9f797df20b310491a743b540bbde9b631c704
Mateusz Kluska <mateusz.marek.kluska@gmail.com>
2022-09-01 16:33:47 +0100

    added LessText and Login

--- src/index.js
+++ src/index.js
@@ -4,18 +4,31 @@ import './index.css';
 import App from './App';
 import Footer from './Components/Footer.js';
 import Clock from './Components/Clock.js';
+import Login from './Components/login.js';
+import LessText from './Components/Less.js'
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App />
     <Footer />
     <Clock />
     <Clock />
+    <Login /> */}
+    <LessText
+      text={`Focused, hard work is the real key
+      to success. Keep your eyes on the goal, 
+      and just keep taking the next step 
+      towards completing it.`}
+      maxLength={35}
+    />,
   </React.StrictMode>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
